75708edd64931ac1a9ceb47faa74b5d3feb55a8e
Pablo Bernal Modino <pablobernal_92@hotmail.com>
2017-04-20 22:44:31 +0200

    Fixed incorrect wave height

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,133 +1,135 @@
 // Initialize configuration
-var Clay = require('pebble-clay');
-var clayConfig = require('./config');
-var clay = new Clay(clayConfig, null, { autoHandleEvents: false });
+var Clay = require('pebble-clay')
+var clayConfig = require('./config')
+var clay = new Clay(clayConfig, null, { autoHandleEvents: false })
 
 // Require the keys' numeric values.
-var keys = require('message_keys');
+var keys = require('message_keys')
 
 var xhrRequest = function (url, type, callback) {
-  var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest()
   xhr.onload = function () {
-    callback(this.responseText);
-  };
-  xhr.open(type, url);
-  xhr.send();
-};
-
-function SendMessageToPebble(dict, messageType){
-  Pebble.sendAppMessage(dict, function() {
-    console.log(messageType + ' sent: ' + JSON.stringify(dict));
-  }, function(e) {
-    console.log('Failed to send ' + messageType);
-    console.log(JSON.stringify(e));
-  });
+    callback(this.responseText)
+  }
+  xhr.open(type, url)
+  xhr.send()
 }
 
-var seaWeedAPIKey = '';
+function SendMessageToPebble (dict, messageType) {
+  Pebble.sendAppMessage(dict, function () {
+    console.log(messageType + ' sent: ' + JSON.stringify(dict))
+  }, function (e) {
+    console.log('Failed to send ' + messageType)
+    console.log(JSON.stringify(e))
+  })
+}
 
-function GetSettings(){
-  var favHour = 12;
-  var spot = 177;
-  var color = 0;  
-  try {    
-    console.log(localStorage.getItem('clay-settings'));
-    var settings = JSON.parse(localStorage.getItem('clay-settings')) || {};
-    if (typeof settings.FavouriteHour != 'undefined'){  
-      favHour = parseInt(settings.FavouriteHour);
-    } 
-    if (typeof settings.Color != 'undefined'){
-      color = parseInt(settings.Color);  
+var seaWeedAPIKey = ''
+
+function GetSettings () {
+  var favHour = 12
+  var spot = 177
+  var color = 0
+  try {
+    console.log(localStorage.getItem('clay-settings'))
+    var settings = JSON.parse(localStorage.getItem('clay-settings')) || {}
+    if (typeof settings.FavouriteHour != 'undefined') {
+      favHour = parseInt(settings.FavouriteHour)
     }
-    if (typeof settings.Spot != 'undefined'){
-      spot = parseInt(settings.Spot); 
+    if (typeof settings.Color != 'undefined') {
+      color = parseInt(settings.Color)
     }
-  } catch (e) {  
-    console.log('Exception Getting settings');
+    if (typeof settings.Spot != 'undefined') {
+      spot = parseInt(settings.Spot)
+    }
+  } catch (e) {
+    console.log('Exception Getting settings')
   }
 
-  var dictionary = {};
-  dictionary[keys.FavouriteHour] = favHour;
-  dictionary[keys.Color] = color;
-  dictionary[keys.Spot] = spot;
+  var dictionary = {}
+  dictionary[keys.FavouriteHour] = favHour
+  dictionary[keys.Color] = color
+  dictionary[keys.Spot] = spot
 
-  return dictionary;
+  return dictionary
 }
 
-function SendSettings(){
-  var dictionary = GetSettings();
-  SendMessageToPebble(dictionary, "SendSettings");
+function SendSettings () {
+  var dictionary = GetSettings()
+  SendMessageToPebble(dictionary, 'SendSettings')
 }
 
-function SendForecast(){
-  var dict = GetSettings();
+function SendForecast () {
+  var dict = GetSettings()
   // Construct URL
-  var url = 'http://magicseaweed.com/api/' + seaWeedAPIKey + 
-      '/forecast/?spot_id=' + dict[keys.Spot] + '&units=eu'+
-      '&fields=timestamp,fadedRating,solidRating,'+
-      'swell.components.combined.*,'+
-      'wind.speed,wind.direction';
-  
+  var url = 'http://magicseaweed.com/api/' + seaWeedAPIKey +
+    '/forecast/?spot_id=' + dict[keys.Spot] + '&units=eu' +
+    '&fields=timestamp,fadedRating,solidRating,' +
+    'swell.minBreakingHeight,' +
+    'swell.maxBreakingHeight,' +
+    'swell.components.primary.direction,' +
+    'swell.components.primary.period,' +
+    'wind.speed,wind.direction'
+
   // Send request to MagicSeaWeed
   xhrRequest(url, 'GET',
     function (responseText) {
       // responseText contains a JSON object with forecast info
-      var json = JSON.parse(responseText);
-      var num = 0;
-      
-      var dictionary = {};
-      dictionary[keys.SwellHeight] = '';
-      var timezoneOffsetHours = new Date().getTimezoneOffset() / 60;
+      var json = JSON.parse(responseText)
+      var num = 0
+
+      var dictionary = {}
+      dictionary[keys.SwellHeight] = ''
+      var timezoneOffsetHours = new Date().getTimezoneOffset() / 60
       // Get only the first 2 forecasts
-      for(var k in json){
-        var date = new Date(json[k].timestamp*1000);     
-        if(date.getHours() == dict[keys.FavouriteHour] - timezoneOffsetHours){
+      for (var k in json) {
+        var date = new Date(json[k].timestamp * 1000)
+        if (date.getHours() == dict[keys.FavouriteHour] - timezoneOffsetHours) {
           // Swell Keys
-          dictionary[keys.FadedRating + num] = json[k].fadedRating;
-          dictionary[keys.SolidRating + num] = json[k].solidRating;
-          dictionary[keys.SwellPeriod + num] = 
-            json[k].swell.components.combined.period;
-          dictionary[keys.SwellHeight] += 
-            json[k].swell.components.combined.height.toString();
-          dictionary[keys.SwellDirection + num] = 
-            Math.round(json[k].swell.components.combined.direction);
+          dictionary[keys.FadedRating + num] = json[k].fadedRating
+          dictionary[keys.SolidRating + num] = json[k].solidRating
+          dictionary[keys.SwellPeriod + num] = json[k].swell.components.primary.period
+          dictionary[keys.SwellHeight] +=
+            (Math.round((json[k].swell.maxBreakingHeight + json[k].swell.minBreakingHeight) / 2 * 10) / 10)
+              .toString()
+          dictionary[keys.SwellDirection + num] = Math.round(json[k].swell.components.primary.direction)
           // Wind Keys
-          dictionary[keys.WindSpeed + num] = json[k].wind.speed;
-          dictionary[keys.WindDirection + num] = json[k].wind.direction;
-          num++;
-          if(num === 2) {
-            break;
+          dictionary[keys.WindSpeed + num] = json[k].wind.speed
+          dictionary[keys.WindDirection + num] = json[k].wind.direction
+          num++
+          if (num === 2) {
+            break
           }
-          dictionary[keys.SwellHeight] += '|';
+          dictionary[keys.SwellHeight] += '|'
         }
       }
 
-      SendMessageToPebble(dictionary, "forecast");
+      SendMessageToPebble(dictionary, 'forecast')
     }
-  );
+  )
 }
 
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready',
   function (e) {
-    console.log('PebbleKit JS ready!');
+    console.log('PebbleKit JS ready!')
     // Get the initial forecast
-    SendForecast();
-});
+    SendForecast()
+  })
 
 // Get AppMessage events
-Pebble.addEventListener('appmessage', function(e) {
-  SendForecast();
-});
+Pebble.addEventListener('appmessage', function (e) {
+  SendForecast()
+})
 
-Pebble.addEventListener('showConfiguration', function(e) {
-  Pebble.openURL(clay.generateUrl());
-});
+Pebble.addEventListener('showConfiguration', function (e) {
+  Pebble.openURL(clay.generateUrl())
+})
 
-Pebble.addEventListener('webviewclosed', function(e) {
+Pebble.addEventListener('webviewclosed', function (e) {
   if (e && !e.response) {
-    return;
+    return
   }
-  clay.getSettings(e.response);
-  SendSettings();
-});
+  clay.getSettings(e.response)
+  SendSettings()
+})
